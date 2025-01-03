c9e03e88b46b385cadb2e5e328b7553c6a3a9b3a
Hardi <hardivirani2003@gmail.com>
2025-01-03 15:25:44 -0500

    First methods

---
+++ src/TV.java
@@ -0,0 +1,23 @@
+public class TV
+{
+
+
+    int channel=1;
+    int volumelevel=1;
+    boolean on = false;
+
+    public void turOn()
+    {
+        on = true;
+    }
+    public void turnOff()
+    {
+        on = false;
+    }
+
+    public static void main(String[]args)
+    {
+        System.out.println("Hello");
+    }
+
+}
